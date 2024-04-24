c3bc461962f19bbb58e81b22de6094a826533386
Andreas <xnorcode@gmail.com>
2024-04-24 01:02:06 +0100

    refactor: speed improvement

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
 import os, sys, json
 import time
+import base64
+
 sys.path.extend(['/stable-diffusion-webui'])
 
 from modules import timer
@@ -41,6 +43,83 @@ class Predictor(BasePredictor):
         from modules.api.models import StableDiffusionTxt2ImgProcessingAPI, StableDiffusionImg2ImgProcessingAPI
         self.StableDiffusionTxt2ImgProcessingAPI = StableDiffusionTxt2ImgProcessingAPI
         self.StableDiffusionImg2ImgProcessingAPI = StableDiffusionImg2ImgProcessingAPI
+
+        file_path = Path("init.jpg")
+        base64_encoded_data = base64.b64encode(file_path.read_bytes())
+        base64_image = base64_encoded_data.decode('utf-8')
+
+        payload = {
+           "override_settings": {
+                "sd_model_checkpoint": "juggernautXL_v9Rdphoto2Lightning.safetensors",
+                "sd_vae": "vae-ft-mse-840000-ema-pruned.safetensors",
+                 "CLIP_stop_at_last_layers": 1,
+            },
+            "override_settings_restore_afterwards": False,
+            "prompt": "office building",
+            "steps": 1,
+            "init_images": [base64_image],
+            "denoising_strength": 0.1,
+            "do_not_save_samples": True,
+            "alwayson_scripts": {
+                "Tiled Diffusion": {
+                    "args": [
+                        True,
+                        "MultiDiffusion",
+                        True,
+                        True,
+                        1,
+                        1,
+                        112,
+                        144,
+                        4,
+                        8,
+                        "4x-UltraSharp",
+                        1.1, 
+                        False, 
+                        0,
+                        0.0, 
+                        3,
+                    ]
+                },
+                "Tiled VAE": {
+                    "args": [
+                        True,
+                        3072,
+                        192,
+                        True,
+                        True,
+                        True,
+                        True,
+                    ]
+
+                },
+                "controlnet": {
+                    "args": [
+                        {
+                            "enabled": True,
+                            "module": "tile_resample",
+                            "model": "control_v11f1e_sd15_tile",
+                            "weight": 0.2,
+                            "image": base64_image,
+                            "resize_mode": 1,
+                            "lowvram": False,
+                            "downsample": 1.0,
+                            "guidance_start": 0.0,
+                            "guidance_end": 1.0,
+                            "control_mode": 1,
+                            "pixel_perfect": True,
+                            "threshold_a": 1,
+                            "threshold_b": 1,
+                            "save_detected_map": False,
+                            "processor_res": 512,
+                        }
+                    ]
+                }
+            }
+        }
+
+        req = StableDiffusionImg2ImgProcessingAPI(**payload)
+        self.api.img2imgapi(req)
 
         print(f"Startup time: {startup_timer.summary()}.")
 
